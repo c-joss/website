07d07cc66114f2a32c0bed7b3d80c4357e3bead8
Courtney Macgregor <cjoss90@hotmail.com>
2024-07-27 03:58:23 +1000

    restyled website

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ const h3 = document.createElement("h3");
 h3.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h3);
+
+
+
+document.getElementById("menuAbout").addEventListener("mouseover",
+    function kitty1() {
+        document.getElementById("cat").style.backgroundImageSource = "url(./resources/images/cat-paw-up.png)"
+    }).false;
+
+kitty1()
+
+
+document.getElementById('menuProjects').addEventListener('mouseover',
+    function kitty2() {
+        document.getElementById('cat').style.backgroundImageSource = url('./resources/images/cat-paw-down.png')inherit;
+    }).false;
+
+
+
