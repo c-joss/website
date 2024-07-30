02023cbb5cd7e6664a921f5b6bc4751ba3ac6f0b
Courtney Macgregor <cjoss90@hotmail.com>
2024-07-31 01:22:51 +1000

    added js functions

--- script.js
+++ script.js
@@ -5,13 +5,44 @@ document.getElementById("Hi").addEventListener("mouseover",
         document.getElementById("ball").style.backgroundPositionY = "0px"
     }, false);
 
+document.getElementById("Hi").addEventListener("mouseout",
+    function kitty1() {
+        document.getElementById("cat").style.backgroundImage = "url(./resources/images/cat.PNG)";
+        document.getElementById("ball").style.backgroundImage = ""; 
+        }, false);   
 
 document.getElementById("Bye").addEventListener('mouseover',
     function kitty2() {
         document.getElementById("cat").style.backgroundImage = "url(./resources/images/cat-paw-down.png)";
         document.getElementById("ball").style.backgroundImage = "url(./resources/images/globe.png)";
-        document.getElementById("ball").style.backgroundPositionY = "100px"
+        document.getElementById("ball").style.backgroundPositionY = "90px"
     }, false);
+
+document.getElementById("Bye").addEventListener("mouseout",
+    function kitty1() {
+        document.getElementById("cat").style.backgroundImage = "url(./resources/images/cat.PNG)";
+        document.getElementById("ball").style.backgroundImage = ""; 
+        }, false);  
+
+function clickBye() {
+    document.getElementById("myPic").style.backgroundImage = "url(./resources/images/sleepingCat.png)";
+    document.getElementById("myPic").style.backgroundPositionX = "50px";
+    var divide = document.getElementById("hello");
+  if (divide.style.display === "none") {
+    divide.style.display = "block";
+  } else {
+    divide.style.display = "none";   
+}}
+
+function clickHi() {
+    document.getElementById("myPic").style.backgroundImage = "url(./resources/images/IMG_2118.jpg)";
+    document.getElementById("myPic").style.backgroundPositionX = "0px";
+    var divide = document.getElementById("hello");
+    if (divide.style.display === "block") {
+      divide.style.display = "none";
+    } else {
+      divide.style.display = "block";   
+}}
 
 
 
